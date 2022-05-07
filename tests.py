0fabeb43eb15a072a8affbf6c19621789d364ab1
williamgl <ligan@oregonstate.edu>
2022-05-07 12:27:48 -0700

    push function to test

--- tests.py
+++ tests.py
@@ -1,9 +1,15 @@
 import unittest
+import task
 
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        pass
+        expected = "Hello World"
+        self.assertEqual(task.my_func(), expected)
+
+    def test2(self):
+        expected = "Bye"
+        self.assertNotEqual(task.my_func(), expected)
 
 
 if __name__ == '__main__':
